e028ecd631bf483acfc3f19e3af77fbc524fa2d0
zemla4ok <dima-vital@live.com>
2018-02-09 15:11:16 +0300

    models

---
+++ models/agents.js
@@ -0,0 +1,13 @@
+module.exports = (Sequelize, sequelize) => {
+    return sequelize.define('agents', {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        tel: Sequelize.STRING,
+        officeId: Sequelize.INTEGER
+    });
+};

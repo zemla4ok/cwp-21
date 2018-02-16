13794c950c4086f8d9b899a25698e5c293ba7262
zemla4ok <dima-vital@live.com>
2018-02-16 09:38:18 +0300

    complete

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const db = require('./context')(Sequelize, config);
 const server = require('./server')(db, config);
 
 (async function() {
-    //await db.sequelize.sync();
+    await db.sequelize.sync();
 
     server.listen(3000, () => console.log('Server is running'));
 })();
